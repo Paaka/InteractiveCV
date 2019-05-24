e66c9dd3aa0a21f02de3450232727fb00ec0f6a0
Matt <Mateuszhartabus@gmail.com>
2019-05-24 10:29:47 +0200

    initial commit

---
+++ myWebsite/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <app-main></app-main>
+    
+  </div>
+</template>
+
+<script>
+import Main from './components/Main.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+    'app-main': Main,
+  }
+}
+</script>
+
+<style lang="scss">
+
+*,
+*::before,
+*::after{
+  margin: 0px;
+  padding: 0px;
+}
+
+html{
+  font-size: 62.5%;
+}
+
+</style>
---
+++ myWebsite/src/components/Main.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="welcome">
+        <div class="welcome__heading">
+             <h1>Hello !</h1>
+        </div>
+        <vue-typer text="My name is Matt and I'm Junior Front-end developer"></vue-typer><br/>
+        <button>Learn More</button>
+    </div>
+</template>
+<script>
+import { VueTyper } from 'vue-typer';
+
+export default {
+    components:{
+        'vue-typer': VueTyper
+    }
+}
+</script>
+
+<style lang="scss">
+.welcome{
+    background-image: url("../assets/Main.jpg");
+    filter:brightness(75%);
+    width: 100%;
+    height:95vh;
+    background-position: center; /* Center the image */
+    background-repeat: no-repeat; /* Do not repeat the image */
+    background-size: cover;
+    color: #eee;
+    font-size:3rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    &__heading{
+        filter: brightness(100%);
+        display: flex;
+        align-items: flex-start;
+        margin-right: 2rem;
+    }
+}
+
+vue-typer{
+    display: block;
+  
+font-size: 3rem;
+font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+
+}
+
+.custom.char.typed{
+        color:white;
+    }
+
+</style>
